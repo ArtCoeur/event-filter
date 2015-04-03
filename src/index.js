e13de6d10e6d6df98e1c87da946292959bacdfa4
Tim Rodger <tim.rodger@gmail.com>
2015-04-03 17:25:21 +0100

    Send object to logstash client, adds debug

--- src/index.js
+++ src/index.js
@@ -12,24 +12,32 @@ function doConnect(){
             'amqp://' + process.env.RABBITMQ_PORT_5672_TCP_ADDR + ':' + process.env.RABBITMQ_PORT_5672_TCP_PORT
         );
 
+        logger.info('env' +
+        ' : proto: ' + process.env.LOGSTASH_PORT_5000_TCP_PROTO +
+        ' : addr: ' + process.env.LOGSTASH_PORT_5000_TCP_ADDR +
+        ' : port: ' + process.env.LOGSTASH_PORT_5000_TCP_PORT);
+
         var logstash = new Logstash({
-            type: 'tcp', // udp, tcp, memory
-            host: process.env.LOGSTASH_PORT_,
-            port: process.env.LOGSTASH_PORT_
+            type: process.env.LOGSTASH_PORT_5000_TCP_PROTO, // udp, tcp, memory
+            host: process.env.LOGSTASH_PORT_5000_TCP_ADDR,
+            port: process.env.LOGSTASH_PORT_5000_TCP_PORT
         });
 
         context.on('ready', function() {
             logger.info('event-filter: connected');
 
-            // subscribe to pub and sub queues
-            var sub = context.socket('SUB'),
-                pub = context.socket('PUB');
+            // subscribe to 'events' queue
+            var sub = context.socket('SUB');
 
             sub.connect('events', function(){
                 // deal with facts as they come in
                 sub.on('data', function (body) {
-                    // send to logstash
-                    logstash.send(body);
+                    var fact = JSON.parse(body);
+                    logger.info('event-filter: fact.name ' + fact.name);
+                    // send fact object to logstash not a string
+                    logstash.send(fact, function(err, result){
+                        logger.info('logstash error: ' + err + ', result: ' + result);
+                    });
                 });
             });
         });
